64a36e862082bef941d56bb7d477c3d2f59d43cb
rcmaddox <65855506+rcmaddox@users.noreply.github.com>
2022-01-26 23:18:56 -0600

    Basic App structure and Carousel Structure that will be used for displaying Pages is complete.

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,17 @@
 import './App.css';
-import Header from "./Header"
+import ResponsiveAppBar from "./Header"
 import Footer from "./Footer"
 import Carousel, { CarouselItem } from "./Storybook";
 
 function App() {
   return (
       <div>
-        <Header />
+        <ResponsiveAppBar />
           <div className="App">
               <Carousel>
                   <CarouselItem> Item 1</CarouselItem>
                   <CarouselItem> Item 2</CarouselItem>
-                  <CarouselItem> Item 31</CarouselItem>
+                  <CarouselItem> Item 3</CarouselItem>
               </Carousel>
           </div>
         <Footer />
--- src/components/Storybook.js
+++ src/components/Storybook.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useSwipeable} from "react-swipeable";
 import './Storybook.css';
 
+///////////// Carousel Items  /////////////////////
 export const CarouselItem = ({ children, width}) => {
     return (
         <div className="carousel-item" style={{width: width}}>
@@ -9,16 +11,96 @@ export const CarouselItem = ({ children, width}) => {
     );
 };
 
+//////////////////// Carousel Body with Buttons and Index  -- useEffects included //////////////////////
+
 const Carousel = ({children}) => {
+    const [activeIndex, setActiveIndex] = useState(0);
+    const [ paused, setPaused] = useState(false);
+
+    const updateIndex = (newIndex) => {
+        if (newIndex < 0) {
+            newIndex = React.Children.count(children) - 1;
+        } else if (newIndex >= React.Children.count(children)) {
+            newIndex = 0;
+        }
+            setActiveIndex(newIndex);
+    };
+
+
+
+useEffect(() => {
+
+    //////////////////// Interval Timer  ////////////////////
+
+    // const interval = setInterval(() => {
+    //     if (!paused) {
+    //         updateIndex(activeIndex + 1);
+    //     }
+    //     updateIndex(activeIndex + 1);
+    // }, 3000);
+    //
+    // return () => {
+    //     if (interval) {
+    //         clearInterval(interval);
+    //     }
+    // };
+
+    ////////////////////// End Timer  /////////////////////
+
+    //////////////////// Swiping for Mobile //////////////
+});
+const handlers = useSwipeable({
+    onSwipedLeft: () => updateIndex(activeIndex + 1),
+    onSwipedRight: () => updateIndex(activeIndex - 1)
+});
+///////////////////// End Swiping  //////////////////////
     return (
-        <div className="carousel">
-            <div className="inner" style={{transform: "translateX(-0%)"}}>
+        <div
+            {...handlers}
+            className="carousel"
+        onMouseEnter={() => setPaused(true)}
+         onMouseLeave={() => setPaused(false)}
+        >
+            <div className="inner"
+                 style={{transform: `translateX(-${activeIndex * 100}%)`   }}
+            >
                 {React.Children.map(children, (child, index) => {
                     return React.cloneElement(child, {width: "100%"});
                 })}
             </div>
+            <div className="indicators">
+                <button
+                    onClick={() => {
+                        updateIndex(activeIndex -1);
+                }}
+                >
+                    Prev
+                </button>
+
+                {React.Children.map(children, (child, index) => {
+                    return (
+                        <button
+                            className={`${index === activeIndex ? "active" : ""}`}
+                            onClick={() => {
+                                updateIndex(index);
+                            }}
+                        >
+                            {index + 1}
+                        </button>
+                    )
+
+                })}
+
+                <button
+                    onClick = {() => {
+                        updateIndex(activeIndex +1);
+                }}
+            >
+            Next
+        </button>
         </div>
-    );
+            </div>
+        );
 };
 
 export default Carousel;
