eab501cbb4cc718aa36bfe2b2131f001f510c9fb
rcmaddox <65855506+rcmaddox@users.noreply.github.com>
2022-01-27 12:02:05 -0600

    API testing in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,45 @@
+import React from 'react';
 import './App.css';
 import ResponsiveAppBar from "./Header"
 import Footer from "./Footer"
 import Carousel, { CarouselItem } from "./Storybook";
+import axios from 'axios';
 
-function App() {
-  return (
-      <div>
-        <ResponsiveAppBar />
-          <div className="App">
-              <Carousel>
-                  <CarouselItem> Item 1</CarouselItem>
-                  <CarouselItem> Item 2</CarouselItem>
-                  <CarouselItem> Item 3</CarouselItem>
-              </Carousel>
-          </div>
-        <Footer />
-      </div>
+///////////////////////// API TEST SECTION ///////////////////////////////////
+const api = axios.create({
+    baseURL: "http://localhost:8000/api/users/"
+})
 
 
-  );
+
+class App extends React.Component {
+
+    // Constructor
+    constructor() {
+        super();
+        api.get('/').then(response => {
+            console.log(response.data)
+        })
+    };
 }
+
+        ///////////////////////////////END TEST SECTION /////////////////////////////////
+// function App() {
+//   return (
+//       <div>
+//         <ResponsiveAppBar />
+//           <div className="App">
+//               <Carousel>
+//                   <CarouselItem> Item 1</CarouselItem>
+//                   <CarouselItem> Item 2</CarouselItem>
+//                   <CarouselItem> Item 3</CarouselItem>
+//               </Carousel>
+//           </div>
+//         <Footer />
+//       </div>
+//
+//
+//   );
+// }
 
 export default App;
